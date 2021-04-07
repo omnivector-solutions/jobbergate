ecd37f3d7b144342a6893d27ddba50808fb3311f
zsinx6 <zsinx6@users.noreply.github.com>
2021-04-07 11:07:35 -0300

    Add file to control the database

---
+++ jobbergateapi2/storage.py
@@ -0,0 +1,16 @@
+import databases
+import sqlalchemy
+
+from jobbergateapi2.apps.users.models import metadata
+from jobbergateapi2.config import settings
+
+database = databases.Database(settings.DATABASE_URL)
+
+
+def create_all_tables():
+    """
+    Create all the tables in the database
+    """
+    engine = sqlalchemy.create_engine(settings.DATABASE_URL)
+
+    metadata.create_all(engine)
