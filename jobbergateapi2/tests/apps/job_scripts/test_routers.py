9467028177ec3f0e30896967dd4acf97065eaa5c
zsinx6 <zsinx6@users.noreply.github.com>
2021-04-23 15:35:51 -0300

    Test for inject_sbatch_params

--- jobbergateapi2/tests/apps/job_scripts/test_routers.py
+++ jobbergateapi2/tests/apps/job_scripts/test_routers.py
@@ -3,6 +3,7 @@ Tests for the /job-scripts/ endpoint.
 """
 import json
 from io import StringIO
+from textwrap import dedent
 from unittest import mock
 
 import nest_asyncio
@@ -12,6 +13,7 @@ from fastapi import status
 
 from jobbergateapi2.apps.applications.models import applications_table
 from jobbergateapi2.apps.applications.schemas import Application
+from jobbergateapi2.apps.job_scripts.routers import inject_sbatch_params
 from jobbergateapi2.apps.users.models import users_table
 from jobbergateapi2.apps.users.schemas import UserCreate
 from jobbergateapi2.storage import database
@@ -21,6 +23,73 @@ from jobbergateapi2.tests.apps.conftest import insert_objects
 # this lib is necessary to avoid the errror "this event loop
 # is already running"
 nest_asyncio.apply()
+
+
+@pytest.fixture
+def job_script_data_as_string():
+    """
+    Example of a default application script.
+    """
+    content = json.dumps(
+        {
+            "application.sh": dedent(
+                """\
+                            #!/bin/bash
+
+                            #SBATCH --job-name=rats
+                            #SBATCH --partition=debug
+                            #SBATCH --output=sample-%j.out
+
+
+                            echo $SLURM_TASKS_PER_NODE
+                            echo $SLURM_SUBMIT_DIR"""
+            )
+        }
+    )
+    return content
+
+
+@pytest.fixture
+def new_job_script_data_as_string():
+    """
+    Example of an application script after the injection of the sbatch params.
+    """
+    content = json.dumps(
+        {
+            "application.sh": dedent(
+                """\
+                            #!/bin/bash
+
+                            #SBATCH --comment=some_comment
+                            #SBATCH --nice=-1
+                            #SBATCH -N 10
+                            #SBATCH --job-name=rats
+                            #SBATCH --partition=debug
+                            #SBATCH --output=sample-%j.out
+
+
+                            echo $SLURM_TASKS_PER_NODE
+                            echo $SLURM_SUBMIT_DIR"""
+            )
+        }
+    )
+    return content
+
+
+@pytest.fixture
+def sbatch_params():
+    """
+    String content of the argument --sbatch-params.
+    """
+    return ["--comment=some_comment", "--nice=-1", "-N 10"]
+
+
+def test_inject_sbatch_params(job_script_data_as_string, sbatch_params, new_job_script_data_as_string):
+    """
+    Test the injection of sbatch params in a default application script.
+    """
+    injected_string = inject_sbatch_params(job_script_data_as_string, sbatch_params)
+    assert injected_string == new_job_script_data_as_string
 
 
 @pytest.mark.asyncio
