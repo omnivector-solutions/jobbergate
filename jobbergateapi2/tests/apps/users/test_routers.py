cc75bb62dc7e2faad2daa41d4253e5317b8ce2f4
zsinx6 <zsinx6@users.noreply.github.com>
2021-05-20 15:46:43 -0300

    Fix tests

--- jobbergateapi2/tests/apps/users/test_routers.py
+++ jobbergateapi2/tests/apps/users/test_routers.py
@@ -1,5 +1,5 @@
 """
-Tests for the /users endpoint
+Tests for the /users endpoint.
 """
 import nest_asyncio
 import pytest
@@ -18,15 +18,15 @@ nest_asyncio.apply()
 
 @pytest.mark.asyncio
 @database.transaction(force_rollback=True)
-async def test_search(client):
+async def test_list(client, user_data):
     """
-    Create a user then test if the search works
+    Create a user then test if the listing works.
     """
 
-    user = [UserCreate(username="user1", email="email1@email.com", password="1" * 12)]
+    user = [UserCreate(id=1, **user_data)]
     await insert_objects(user, users_table)
 
-    response = client.get(f"/users/?q={user[0].username}")
+    response = client.get("/users/")
     assert response.status_code == status.HTTP_200_OK
 
     data = response.json()
@@ -35,18 +35,17 @@ async def test_search(client):
 
 @pytest.mark.asyncio
 @database.transaction(force_rollback=True)
-async def test_search_with_pagination_limit_offset(client):
+async def test_list_with_pagination_limit_offset(client):
     """
-    Test if the pagination is working throught the endpoint with 2 users
+    Test if the pagination is working throught the endpoint with 2 users.
     """
     users = [
-        UserCreate(username="user1", email="email1@email.com", password="1" * 12),
-        UserCreate(username="user2", email="email2@email.com", password="1" * 12),
+        UserCreate(full_name="user1", email="email1@email.com", password="1" * 12),
+        UserCreate(full_name="user2", email="email2@email.com", password="1" * 12),
     ]
     await insert_objects(users, users_table)
 
-    user_name = "user1"
-    response = client.get(f"/users/?q={user_name}&limit=1&offset=0")
+    response = client.get("/users/?limit=1&offset=0")
     assert response.status_code == status.HTTP_200_OK
 
     data = response.json()
@@ -55,11 +54,12 @@ async def test_search_with_pagination_limit_offset(client):
     assert count[0][0] == 2
 
 
-def test_search_without_results(client):
+@pytest.mark.asyncio
+async def test_list_without_results(client):
     """
-    Test when there is no user matching the search criteria
+    Test when there is no user.
     """
-    response = client.get("/users/?name=Some Name")
+    response = client.get("/users/")
 
     assert response.status_code == status.HTTP_200_OK
 
@@ -71,7 +71,7 @@ def test_search_without_results(client):
 @database.transaction(force_rollback=True)
 async def test_create_user(client, user_data):
     """
-    Test user creation
+    Test user creation, should be active and not superuser.
     """
     response = client.post("/users/", json=user_data)
     assert response.status_code == status.HTTP_200_OK
@@ -79,14 +79,15 @@ async def test_create_user(client, user_data):
     assert count[0][0] == 1
 
     user = User.parse_obj(await database.fetch_one(users_table.select()))
-    assert user.is_admin is False
+    assert user.is_superuser is False
+    assert user.is_active is True
 
 
 @pytest.mark.asyncio
 @database.transaction(force_rollback=True)
 async def test_create_user_duplication(client, user_data):
     """
-    Test the case where there is a violation in the database constraints for unique
+    Test the case where there is a violation in the database constraints for unique.
     """
     response = client.post("/users/", json=user_data)
     assert response.status_code == status.HTTP_200_OK
@@ -99,15 +100,56 @@ async def test_create_user_duplication(client, user_data):
 
 @pytest.mark.asyncio
 @database.transaction(force_rollback=True)
-async def test_create_user_admin(client, user_data):
+async def test_create_superuser(client, user_data):
     """
-    Test creating the user with the admin permission
+    Test create a superuser.
     """
-    user_data["is_admin"] = True
+    user_data["is_superuser"] = True
     response = client.post("/users/", json=user_data)
     assert response.status_code == status.HTTP_200_OK
     count = await database.fetch_all("SELECT COUNT(*) FROM users")
     assert count[0][0] == 1
 
     user = User.parse_obj(await database.fetch_one(users_table.select()))
-    assert user.is_admin is True
+    assert user.is_superuser is True
+
+
+@pytest.mark.asyncio
+@database.transaction(force_rollback=True)
+async def test_users_get(client, user_data):
+    """
+    Test getting a user by id.
+    """
+    user = [UserCreate(id=1, **user_data)]
+    await insert_objects(user, users_table)
+
+    response = client.get("/users/1")
+    assert response.status_code == status.HTTP_200_OK
+
+    data = response.json()
+    assert data["email"] == user_data["email"]
+
+
+@pytest.mark.asyncio
+async def test_users_get_empty(client):
+    """
+    Try to get the user of id=1 which doesn't exists and will return 404.
+    """
+    response = client.get("/users/1")
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.asyncio
+@database.transaction(force_rollback=True)
+async def test_users_me(client, user_data):
+    """
+    Test the /user/me endpoint, should return the current authenticated user.
+    """
+    user = [UserCreate(id=1, **user_data)]
+    await insert_objects(user, users_table)
+
+    response = client.get("/user/me")
+    assert response.status_code == status.HTTP_200_OK
+
+    data = response.json()
+    assert data["email"] == user_data["email"]
--- jobbergateapi2/tests/test_pagination.py
+++ jobbergateapi2/tests/test_pagination.py
@@ -10,7 +10,7 @@ def test_string_conversion():
     """
     pagination = Pagination()
 
-    assert str(pagination) == f"q: {pagination.q}, limit: {pagination.limit}, offset: {pagination.offset}"
+    assert str(pagination) == f"limit: {pagination.limit}, skip: {pagination.skip}"
 
 
 def test_pagination_with_default_values():
@@ -19,17 +19,15 @@ def test_pagination_with_default_values():
     """
     pagination = Pagination()
 
-    assert pagination.q is None
     assert pagination.limit == 10
-    assert pagination.offset == 0
+    assert pagination.skip == 0
 
 
 def test_pagination_with_custom_values():
     """
     Test the pagination with custom values
     """
-    pagination = Pagination(q="value", limit=100, offset=1)
+    pagination = Pagination(limit=100, skip=1)
 
-    assert pagination.q == "value"
     assert pagination.limit == 100
-    assert pagination.offset == 1
+    assert pagination.skip == 1
