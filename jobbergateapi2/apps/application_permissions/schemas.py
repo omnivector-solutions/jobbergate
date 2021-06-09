0b830f934ed6b08268051f6ce0b9cff9f3aa7f77
zsinx6 <zsinx6@users.noreply.github.com>
2021-06-09 15:29:34 -0300

    Add ApplicationPermission

---
+++ jobbergateapi2/apps/application_permissions/schemas.py
@@ -0,0 +1,21 @@
+"""
+Defines the schema for the resource ApplicationPermission.
+"""
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class ApplicationPermission(BaseModel):
+    """
+    Request model for the ApplicationPermission resource.
+    """
+
+    id: Optional[int] = Field(None)
+    acl: str = Field(...)
+
+    class Config:
+        orm_mode = True
+
+    def __str__(self):
+        return self.acl
