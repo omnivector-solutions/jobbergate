8e9d65e0abbed949d6153a7af64cf867b89ba172
zsinx6 <zsinx6@users.noreply.github.com>
2021-06-10 10:37:34 -0300

    Improve tests for application

--- jobbergateapi2/tests/apps/applications/test_routers.py
+++ jobbergateapi2/tests/apps/applications/test_routers.py
@@ -51,6 +51,16 @@ async def test_create_application(boto3_client_mock, application_data, client, u
     count = await database.fetch_all("SELECT COUNT(*) FROM applications")
     assert count[0][0] == 1
 
+    query = applications_table.select(applications_table.c.id == 1)
+    application = Application.parse_obj(await database.fetch_one(query))
+
+    assert application is not None
+    assert application.application_name == application_data["application_name"]
+    assert application.application_owner_id == 1
+    assert application.application_config == application_data["application_config"]
+    assert application.application_file == application_data["application_file"]
+    assert application.application_description == ""
+
 
 @pytest.mark.asyncio
 @mock.patch("jobbergateapi2.apps.applications.routers.boto3")
@@ -175,6 +185,34 @@ async def test_delete_application(boto3_client_mock, client, user_data, applicat
 @pytest.mark.asyncio
 @mock.patch("jobbergateapi2.apps.applications.routers.boto3")
 @database.transaction(force_rollback=True)
+async def test_delete_application_bad_permission(boto3_client_mock, client, user_data, application_data):
+    """
+    Test that it is not possible to delete application without proper permission.
+
+    This test proves that an application is not deleted via a DELETE request to the /applciations/<id>
+    endpoint. We show this by asserting that the application still exists in the database after the delete
+    request is made, the correct status code is returned and the boto3 method is never called.
+    """
+    s3_client_mock = mock.Mock()
+    boto3_client_mock.client.return_value = s3_client_mock
+    user = [UserCreate(id=1, **user_data)]
+    await insert_objects(user, users_table)
+
+    application = [Application(application_owner_id=1, **application_data)]
+    await insert_objects(application, applications_table)
+    count = await database.fetch_all("SELECT COUNT(*) FROM applications")
+    assert count[0][0] == 1
+
+    response = client.delete("/applications/1")
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    count = await database.fetch_all("SELECT COUNT(*) FROM applications")
+    assert count[0][0] == 1
+    s3_client_mock.delete_object.assert_not_called()
+
+
+@pytest.mark.asyncio
+@mock.patch("jobbergateapi2.apps.applications.routers.boto3")
+@database.transaction(force_rollback=True)
 async def test_delete_application_not_found(boto3_client_mock, client, user_data, application_data):
     """
     Test DELETE /applications/<id> the correct response code when the application doesn't exist.
@@ -282,6 +320,26 @@ async def test_get_application_by_id_invalid(client, user_data):
 
 @pytest.mark.asyncio
 @database.transaction(force_rollback=True)
+async def test_get_application_by_id_bad_permission(client, user_data, application_data):
+    """
+    Test that it is not possible to get application without proper permission.
+
+    This test proves that GET /application/<id> returns the correct response code when the
+    user don't have the proper permission. We show this by asserting that the status code
+    returned is what we would expect (403).
+    """
+    user = [UserCreate(id=1, **user_data)]
+    await insert_objects(user, users_table)
+
+    application = [Application(id=1, application_owner_id=1, **application_data)]
+    await insert_objects(application, applications_table)
+
+    response = client.get("/applications/1")
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.asyncio
+@database.transaction(force_rollback=True)
 async def test_get_application_all_from_user(client, user_data, application_data):
     """
     Test GET /applications returns only applications owned by the user making the request.
@@ -293,7 +351,7 @@ async def test_get_application_all_from_user(client, user_data, application_data
     user = [UserCreate(id=1, **user_data)]
     await insert_objects(user, users_table)
 
-    application_permission = [ApplicationPermission(id=1, acl="Allow|role:admin|view")]
+    application_permission = [ApplicationPermission(id=1, acl="Allow|Authenticated|view")]
     await insert_objects(application_permission, application_permissions_table)
 
     applications = [
@@ -312,6 +370,32 @@ async def test_get_application_all_from_user(client, user_data, application_data
     assert len(data) == 2
     assert data[0]["id"] == 1
     assert data[1]["id"] == 2
+
+
+@pytest.mark.asyncio
+@database.transaction(force_rollback=True)
+async def test_get_application_all_from_user_bad_permission(client, user_data, application_data):
+    """
+    Test that it is not possible to list applications without proper permission.
+
+    This test proves that the GET /applications returns 403 as status code in the response.
+    We show this by making a request with an user without creating the permission, and then asserting the
+    status code in the response.
+    """
+    user = [UserCreate(id=1, **user_data)]
+    await insert_objects(user, users_table)
+
+    applications = [
+        Application(id=1, application_owner_id=1, **application_data),
+        Application(id=2, application_owner_id=1, **application_data),
+        Application(id=3, application_owner_id=999, **application_data),
+    ]
+    await insert_objects(applications, applications_table)
+    count = await database.fetch_all("SELECT COUNT(*) FROM applications")
+    assert count[0][0] == 3
+
+    response = client.get("/applications/")
+    assert response.status_code == status.HTTP_403_FORBIDDEN
 
 
 @pytest.mark.asyncio
@@ -420,3 +504,55 @@ async def test_update_application(boto3_client_mock, client, user_data, applicat
     assert data["application_description"] == application_data["application_description"]
 
     s3_client_mock.put_object.assert_called_once()
+
+    query = applications_table.select(applications_table.c.id == 1)
+    application = Application.parse_obj(await database.fetch_one(query))
+
+    assert application is not None
+    assert application.application_name == application_data["application_name"]
+    assert application.application_owner_id == 1
+    assert application.application_config == application_data["application_config"]
+    assert application.application_file == application_data["application_file"]
+    assert application.application_description == application_data["application_description"]
+
+
+@pytest.mark.asyncio
+@mock.patch("jobbergateapi2.apps.applications.routers.boto3")
+@database.transaction(force_rollback=True)
+async def test_update_application_bad_permission(boto3_client_mock, client, user_data, application_data):
+    """
+    Test that it is not possible to update applications without proper permission.
+
+    This test proves that an application's values are not updated following a PUT request to the
+    /application/<id> endpoint by a user without permission. We show this by asserting that the s3_client is
+    not called, the status code 403 is returned and that the application_data is still the same as before.
+    """
+    s3_client_mock = mock.Mock()
+    boto3_client_mock.client.return_value = s3_client_mock
+    file_mock = mock.MagicMock(wraps=StringIO("test"))
+    user = [UserCreate(id=1, **user_data)]
+    await insert_objects(user, users_table)
+
+    applications = [
+        Application(
+            id=1, application_owner_id=1, application_description="old description", **application_data
+        ),
+    ]
+    await insert_objects(applications, applications_table)
+    count = await database.fetch_all("SELECT COUNT(*) FROM applications")
+    assert count[0][0] == 1
+
+    application_data["application_name"] = "new_name"
+    application_data["application_description"] = "new_description"
+    response = client.put("/applications/1", data=application_data, files={"upload_file": file_mock})
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    s3_client_mock.put_object.assert_not_called()
+
+    query = applications_table.select(applications_table.c.id == 1)
+    application = Application.parse_obj(await database.fetch_one(query))
+
+    assert application is not None
+    assert application.application_name != application_data["application_name"]
+    assert application.application_owner_id == 1
+    assert application.application_description == "old description"
