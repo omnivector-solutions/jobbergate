ec7467c3b5d8fde05665633785022fe4982f3401
zsinx6 <zsinx6@users.noreply.github.com>
2021-08-13 05:47:59 -0300

    Add example

--- jobbergateapi2/config.py
+++ jobbergateapi2/config.py
@@ -16,6 +16,7 @@ class Settings(BaseSettings):
     SERVERLESS_STAGE: str = Field("staging")
     SERVERLESS_REGION: str = Field("eu-north-1")
     S3_BASE_PATH: str = Field("jobbergate-resources")
+    # BACKEND_CORS_ORIGINS example: "['https://example1.com', 'https://example2.com']"
     BACKEND_CORS_ORIGINS: str = Field("[]")
 
     class Config:
