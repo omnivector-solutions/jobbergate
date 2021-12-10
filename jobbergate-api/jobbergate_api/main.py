63afa162b4e0444d9f5c247c3811e7ee50274bda
matheushent <me@mtosta.dev>
2021-12-10 16:44:13 -0400

    format code

--- jobbergate-api/jobbergate_api/main.py
+++ jobbergate-api/jobbergate_api/main.py
@@ -17,11 +17,7 @@ from jobbergate_api.config import settings
 
 subapp = FastAPI()
 subapp.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
 )
 
 if settings.SENTRY_DSN:
