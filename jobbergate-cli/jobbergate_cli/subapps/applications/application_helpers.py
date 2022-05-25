629c2b4d1a84c003d0a841db67acba07e7c75f13
NilesDev <55181998+NilesDev@users.noreply.github.com>
2022-05-25 15:09:36 +0200

    Making file lister case-insensitive
    Could possibly make case-sensitivity optional, only difference should be the flag in re.compile: 0 vs re.IGNORECASE.

--- jobbergate-cli/jobbergate_cli/subapps/applications/application_helpers.py
+++ jobbergate-cli/jobbergate_cli/subapps/applications/application_helpers.py
@@ -4,7 +4,7 @@ Helper functions that may be used inside of Jobbergate applications.
 import shlex
 import subprocess
 from getpass import getuser
-from pathlib import Path
+import os, re, fnmatch
 
 
 def get_running_jobs(user_only=True):
@@ -27,11 +27,11 @@ def get_running_jobs(user_only=True):
     return ID_alternatives
 
 
-def get_file_list(path=None, search_term="*.*"):
+def get_file_list(path=".", search_term="*.*"):
     """Returns a list of input files in a directory ( default: pwd)."""
-    if not path:
-        path = Path.cwd()
+    all_paths = os.listdir(path)
+    pattern = re.compile(fnmatch.translate(search_term), re.IGNORECASE)
 
-    file_paths = sorted(path.glob(search_term), key=lambda p: p.stat().st_mtime, reverse=True)
-    file_names = [f.name for f in file_paths if f.is_file()]
-    return file_names
+    file_paths = [p for p in all_paths if os.path.isfile(os.path.join(path, p)) and re.match(pattern, p)]
+    file_paths = sorted(file_paths, key=lambda p: os.stat(os.path.join(path, p)).st_mtime, reverse=True)
+    return file_paths
