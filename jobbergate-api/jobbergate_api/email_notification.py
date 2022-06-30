cae511c09f90099182a0d5fef7aeed6b6dee6705
fschuch <felipe@omnivector.solutions>
2022-06-30 15:03:39 -0300

    ARMADA-536 Docstrings and unitest for email sys

--- jobbergate-api/jobbergate_api/email_notification.py
+++ jobbergate-api/jobbergate_api/email_notification.py
@@ -2,28 +2,46 @@
 Email notification system for Jobbergate.
 """
 from dataclasses import dataclass
-from typing import List, Union
+from typing import List, Optional, Union
 
 from buzz import Buzz
 from loguru import logger
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+from sendgrid import SendGridAPIClient  # type: ignore # no type hints or library stubs
+from sendgrid.helpers.mail import Mail  # type: ignore # no type hints or library stubs
 
 from jobbergate_api.config import settings
 
 
 class EmailNotificationError(Buzz):
+    """
+    Custom error to be raised for problems at the email notification system.
+    """
+
     pass
 
 
 @dataclass
 class EmailManager:
+    """
+    Email manager.
+    """
 
     email_client: SendGridAPIClient
-    from_email: str
+    from_email: Optional[str] = ""
 
-    async def send_email(self, to_emails: str, subject: str, html_content: str, **kwargs) -> None:
+    def send_email(self, to_emails: Union[str, List[str]], subject: str, **kwargs) -> None:
+        """
+        Send an email using this manager.
+        """
+        message = self._build_message(to_emails, subject, **kwargs)
 
+        with EmailNotificationError.handle_errors("Error while sending email"):
+            self.email_client.send(message)
+
+    def _build_message(self, to_emails: Union[str, List[str]], subject: str, **kwargs) -> Mail:
+        """
+        Build an email message.
+        """
         EmailNotificationError.require_condition(self.from_email, "The value from_email is empty.")
 
         with EmailNotificationError.handle_errors("Error while creating the message"):
@@ -31,24 +49,22 @@ class EmailManager:
                 from_email=self.from_email,
                 to_emails=to_emails,
                 subject=subject,
-                html_content=html_content,
                 **kwargs,
             )
-
-        with EmailNotificationError.handle_errors("Error while sending email"):
-            response = await self.email_client.send(message)
-        logger.trace(f"Response status code after sending the email: {response.status_code}")
+        return message
 
 
 def notify_submission_aborted(
     job_submission_id: Union[str, int], report_message: str, to_emails: Union[str, List[str]]
 ) -> None:
+    """
+    Notify an email or a list of emails about a job submission that has been aborted.
+    """
     subject = f"Job Submission Aborted (id={job_submission_id})"
-    html_content = f"<strong>{report_message}</strong>"
 
     logger.debug(f"Notifying {to_emails=} that {job_submission_id=} was aborted: {report_message=}")
     try:
-        email_manager.send_email(to_emails, subject, html_content)
+        email_manager.send_email(to_emails, subject, plain_text_content=report_message)
     except EmailNotificationError:
         logger.debug("Email not sent!")
 
