b9cc30cda2ca2fc2e6e7f58cea674752c7138689
matheushent <me@mtosta.dev>
2022-07-11 18:05:27 -0700

    implement support for multi-domain auth in the API

--- jobbergate-api/jobbergate_api/security.py
+++ jobbergate-api/jobbergate_api/security.py
@@ -3,19 +3,47 @@ Instantiates armasec resources for auth on api endpoints using project settings.
 
 Also provides a factory function for TokenSecurity to reduce boilerplate.
 """
-from typing import Optional
+from typing import List, Optional
 
 from armasec import Armasec, TokenPayload
+from armasec.schemas import DomainConfig
 from loguru import logger
 from pydantic import BaseModel, EmailStr
 
 from jobbergate_api.config import settings
 
-guard = Armasec(
-    settings.ARMASEC_DOMAIN,
-    audience=settings.ARMASEC_AUDIENCE,
-    debug_logger=logger.debug if settings.ARMASEC_DEBUG else None,
-)
+
+def get_domain_configs() -> List[DomainConfig]:
+    """
+    Return a list of DomainConfig objects based on the input variables for the Settings class.
+    """
+    domain_configs = [
+        DomainConfig(
+            domain=settings.ARMASEC_DOMAIN,
+            audience=settings.ARMASEC_AUDIENCE,
+            debug_logger=logger.debug if settings.ARMASEC_DEBUG else None,
+        )
+    ]
+    if all(
+        [
+            settings.ARMASEC_ADMIN_DOMAIN,
+            settings.ARMASEC_ADMIN_AUDIENCE,
+            settings.ARMASEC_ADMIN_MATCH_KEY,
+            settings.ARMASEC_ADMIN_MATCH_VALUE,
+        ]
+    ):
+        domain_configs.append(
+            DomainConfig(
+                domain=settings.ARMASEC_ADMIN_DOMAIN,
+                audience=settings.ARMASEC_ADMIN_AUDIENCE,
+                match_keys={settings.ARMASEC_ADMIN_MATCH_KEY: settings.ARMASEC_ADMIN_MATCH_VALUE},
+                debug_logger=logger.debug if settings.ARMASEC_DEBUG else None,
+            )
+        )
+    return domain_configs
+
+
+guard = Armasec(domain_configs=get_domain_configs())
 
 
 class IdentityClaims(BaseModel):
---
+++ jobbergate-api/jobbergate_api/tests/test_security.py
@@ -0,0 +1,47 @@
+"""
+Test the security module.
+"""
+from unittest.mock import patch
+
+from jobbergate_api import security
+
+
+def test_get_domain_configs__loads_only_base_settings():
+    """Check if the correct domain configuration is loaded when only one domain is provided."""
+    with patch.object(security.settings, "ARMASEC_DOMAIN", new="foo.io"):
+        with patch.object(security.settings, "ARMASEC_AUDIENCE", new="https://bar.dev"):
+            domain_configs = security.get_domain_configs()
+
+    assert len(domain_configs) == 1
+    first_config = domain_configs.pop()
+    assert first_config.domain == "foo.io"
+    assert first_config.audience == "https://bar.dev"
+
+
+def test_get_domain_configs__loads_admin_settings_if_all_are_present():
+    """Check if the correct domain configuration is loaded when two domains are provided."""
+    with patch.object(security.settings, "ARMASEC_DOMAIN", new="foo.io"):
+        with patch.object(security.settings, "ARMASEC_AUDIENCE", new="https://bar.dev"):
+            with patch.object(security.settings, "ARMASEC_ADMIN_DOMAIN", new="admin.io"):
+                domain_configs = security.get_domain_configs()
+
+    assert len(domain_configs) == 1
+    first_config = domain_configs.pop()
+    assert first_config.domain == "foo.io"
+    assert first_config.audience == "https://bar.dev"
+
+    with patch.object(security.settings, "ARMASEC_DOMAIN", new="foo.io"):
+        with patch.object(security.settings, "ARMASEC_AUDIENCE", new="https://bar.dev"):
+            with patch.object(security.settings, "ARMASEC_ADMIN_DOMAIN", new="admin.io"):
+                with patch.object(security.settings, "ARMASEC_ADMIN_AUDIENCE", new="https://admin.dev"):
+                    with patch.object(security.settings, "ARMASEC_ADMIN_MATCH_KEY", new="foo"):
+                        with patch.object(security.settings, "ARMASEC_ADMIN_MATCH_VALUE", new="bar"):
+                            domain_configs = security.get_domain_configs()
+
+    assert len(domain_configs) == 2
+    (first_config, second_config) = domain_configs
+    assert first_config.domain == "foo.io"
+    assert first_config.audience == "https://bar.dev"
+    assert second_config.domain == "admin.io"
+    assert second_config.audience == "https://admin.dev"
+    assert second_config.match_keys == dict(foo="bar")
