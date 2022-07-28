351aa4cac13633efdb457fa15806010a8a0ab62a
fschuch <felipe@omnivector.solutions>
2022-07-28 14:11:30 -0300

    ARMADA-683 add dummy files to support the tests

--- jobbergate-api/jobbergate_api/tests/test_file_validation.py
+++ jobbergate-api/jobbergate_api/tests/test_file_validation.py
@@ -1,6 +1,57 @@
 import pytest
 
-from jobbergate_api.file_validation import is_valid_python_file, is_valid_yaml_file
+DUMMY_TEMPLATE = """
+#!/bin/bash
+
+#SBATCH --job-name={{data.job_name}}
+#SBATCH --partition={{data.partition}}
+#SBATCH --output=sample-%j.out
+
+
+echo $SLURM_TASKS_PER_NODE
+echo $SLURM_SUBMIT_DIR
+"""
+
+DUMMY_APPLICATION_SOURCE_FILE = """
+from jobbergate_cli.application_base import JobbergateApplicationBase
+from jobbergate_cli import appform
+
+class JobbergateApplication(JobbergateApplicationBase):
+
+    def mainflow(self, data):
+        questions = []
+
+        questions.append(appform.List(
+            variablename="partition",
+            message="Choose slurm partition:",
+            choices=self.application_config['partitions'],
+        ))
+
+        questions.append(appform.Text(
+            variablename="job_name",
+            message="Please enter a jobname",
+            default=self.application_config['job_name']
+        ))
+        return questions
+"""
+
+DUMMY_APPLICATION_CONFIG = """
+application_config:
+  job_name: rats
+  partitions:
+  - debug
+  - partition1
+jobbergate_config:
+  default_template: test_job_script.sh
+  output_directory: .
+  supporting_files:
+  - test_job_script.sh
+  supporting_files_output_name:
+    test_job_script.sh:
+    - support_file_b.py
+  template_files:
+  - templates/test_job_script.sh
+"""
 
 
 @pytest.mark.parametrize(
