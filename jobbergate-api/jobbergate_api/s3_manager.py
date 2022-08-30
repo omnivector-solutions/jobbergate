99b537fe40148bb0c938a611e6bc74f479e45baf
fschuch <felipe@omnivector.solutions>
2022-08-30 17:00:25 -0300

    add JobScriptFiles to handle supporting files

--- jobbergate-api/jobbergate_api/s3_manager.py
+++ jobbergate-api/jobbergate_api/s3_manager.py
@@ -17,7 +17,8 @@ from file_storehouse import (
     TransformationCodecs,
 )
 from loguru import logger
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, root_validator
+from buzz import require_condition
 
 from jobbergate_api.config import settings
 from jobbergate_api.file_validation import perform_all_checks_on_uploaded_files
@@ -26,6 +27,10 @@ APPLICATIONS_WORK_DIR = "applications"
 APPLICATION_CONFIG_FILE_NAME = "jobbergate.yaml"
 APPLICATION_SOURCE_FILE_NAME = "jobbergate.py"
 APPLICATION_TEMPLATE_FOLDER = "templates"
+
+JOBSCRIPTS_WORK_DIR = "job-scripts"
+JOBSCRIPTS_MAIN_FILE_FOLDER = "main-file"
+JOBSCRIPTS_SUPPORTING_FILES_FOLDER = "supporting-files"
 
 IO_TRANSFORMATIONS = (TransformationCodecs("utf-8"),)
 """
@@ -94,7 +99,7 @@ s3man_jobscripts_factory: Callable[[int], FileManager] = partial(
     file_manager_factory,
     s3_client=s3_client,
     bucket_name=settings.S3_BUCKET_NAME,
-    work_directory=Path("job-scripts"),
+    work_directory=Path(JOBSCRIPTS_WORK_DIR),
     manager_cls=FileManager,
     transformations=IO_TRANSFORMATIONS,
 )
@@ -195,6 +200,75 @@ class ApplicationFiles(BaseModel):
         logger.debug("Success getting application files from the uploaded files")
 
         return application_files
+
+
+class JobScriptFiles(BaseModel):
+    """
+    Model containing job-script files.
+    """
+
+    main_file_path: Path
+    files: Dict[Path, str]
+
+    @root_validator(pre=False, skip_on_failure=True)
+    def check_main_file_path_is_in_files_keys(cls, values):
+        if values["main_file_path"] not in values["files"].keys():
+            raise ValueError("main_file_path is not a valid key on the dict files")
+        return values
+
+    @classmethod
+    def get_from_s3(cls, job_script_id: int):
+        """
+        Alternative method to initialize the model getting the objects from S3.
+        """
+        logger.debug(f"Getting job-script files from S3: {job_script_id=}")
+        file_manager = s3man_jobscripts_factory(job_script_id)
+
+        files = {}
+        main_file_path = None
+        main_file_counter = 0
+
+        for s3_path in file_manager.keys():
+            foldername = s3_path.parts[0]
+            dict_path = s3_path.relative_to(foldername)
+            if foldername == JOBSCRIPTS_MAIN_FILE_FOLDER:
+                files[dict_path] = file_manager.get(s3_path)
+                main_file_counter += 1
+                main_file_path = dict_path
+            elif foldername == JOBSCRIPTS_SUPPORTING_FILES_FOLDER:
+                files[dict_path] = file_manager.get(s3_path)
+
+        require_condition(
+            main_file_counter == 1,
+            f"One main file is expected for a job-script, found {main_file_counter}",
+            ValueError,
+        )
+
+        return cls(main_file_path=main_file_path, files=files)
+
+    @classmethod
+    def delete_from_s3(cls, job_script_id: int):
+        """
+        Deleted the files associated with the given id.
+        """
+        logger.debug(f"Deleting from S3 the files associated to {job_script_id=}")
+        file_manager = s3man_jobscripts_factory(job_script_id)
+        file_manager.clear()
+        logger.debug(f"Files were deleted for {job_script_id=}")
+
+    def write_to_s3(self, job_script_id: int):
+        logger.debug(f"Writing job-script files to S3: {job_script_id=}")
+
+        file_manager = s3man_jobscripts_factory(job_script_id)
+
+        for dict_path, content in self.files.items():
+            if dict_path == self.main_file_path:
+                s3_path = JOBSCRIPTS_MAIN_FILE_FOLDER / dict_path
+            else:
+                s3_path = JOBSCRIPTS_SUPPORTING_FILES_FOLDER / dict_path
+            file_manager[s3_path] = content
+
+        logger.debug("Done writing job-script files to S3")
 
 
 s3man_jobscripts = FileManager(
--- jobbergate-api/jobbergate_api/tests/test_s3_manager.py
+++ jobbergate-api/jobbergate_api/tests/test_s3_manager.py
@@ -16,7 +16,11 @@ from jobbergate_api.s3_manager import (
     APPLICATION_SOURCE_FILE_NAME,
     APPLICATION_TEMPLATE_FOLDER,
     APPLICATIONS_WORK_DIR,
+    JOBSCRIPTS_MAIN_FILE_FOLDER,
+    JOBSCRIPTS_SUPPORTING_FILES_FOLDER,
+    JOBSCRIPTS_WORK_DIR,
     ApplicationFiles,
+    JobScriptFiles,
     engine_factory,
 )
 
@@ -269,3 +273,139 @@ class TestApplicationFiles:
         actual_application_files = ApplicationFiles.get_from_s3(application_id)
 
         assert desired_application_files == actual_application_files
+
+
+class TestJobScriptFiles:
+    def test_check_main_file_path_is_in_files_keys__success(self):
+        jobscript_files = JobScriptFiles(
+            main_file_path="jobbergate.sh", files={"jobbergate.sh": "dummy-file-content"}
+        )
+
+        assert isinstance(jobscript_files, JobScriptFiles)
+
+    def test_check_main_file_path_is_in_files_keys__error_main_file_path(self):
+        with pytest.raises(ValueError):
+            JobScriptFiles(
+                main_file_path="wrong-filename",
+                files={"jobbergate.sh": "dummy-file-content"},
+            )
+
+    def test_get_from_s3(self, mocked_file_manager_factory):
+
+        job_script_id = 1
+
+        work_dir = mocked_file_manager_factory / JOBSCRIPTS_WORK_DIR / str(job_script_id)
+        output_dir = Path("test-output-dir")
+
+        main_file_name = output_dir / "jobbergate.sh"
+        main_file_content = "test-main-file"
+        main_file_path = work_dir / JOBSCRIPTS_MAIN_FILE_FOLDER / main_file_name
+        main_file_path.parent.mkdir(parents=True, exist_ok=True)
+        main_file_path.write_text(main_file_content)
+
+        supporting_file_name = output_dir / "support.sh"
+        supporting_file_content = "test-support-file"
+        supporting_file_path = work_dir / JOBSCRIPTS_SUPPORTING_FILES_FOLDER / supporting_file_name
+        supporting_file_path.parent.mkdir(parents=True, exist_ok=True)
+        supporting_file_path.write_text(supporting_file_content)
+
+        desired_jobscript_files = JobScriptFiles(
+            main_file_path=main_file_name,
+            files={
+                main_file_name: main_file_content,
+                supporting_file_name: supporting_file_content,
+            },
+        )
+
+        actual_jobscript_files = JobScriptFiles.get_from_s3(job_script_id)
+
+        assert desired_jobscript_files == actual_jobscript_files
+
+    def test_get_from_s3__no_main_file(self, mocked_file_manager_factory):
+        job_script_id = 1
+        with pytest.raises(
+            ValueError,
+            match="One main file is expected for a job-script, found 0",
+        ):
+            JobScriptFiles.get_from_s3(job_script_id)
+
+    def test_delete_from_s3(self, mocked_file_manager_factory):
+
+        job_script_id = 1
+
+        work_dir = mocked_file_manager_factory / JOBSCRIPTS_WORK_DIR / str(job_script_id)
+        output_dir = Path("test-output-dir")
+
+        main_file_name = output_dir / "jobbergate.sh"
+        main_file_content = "test-main-file"
+        main_file_path = work_dir / JOBSCRIPTS_MAIN_FILE_FOLDER / main_file_name
+        main_file_path.parent.mkdir(parents=True, exist_ok=True)
+        main_file_path.write_text(main_file_content)
+
+        supporting_file_name = output_dir / "support.sh"
+        supporting_file_content = "test-support-file"
+        supporting_file_path = work_dir / JOBSCRIPTS_SUPPORTING_FILES_FOLDER / supporting_file_name
+        supporting_file_path.parent.mkdir(parents=True, exist_ok=True)
+        supporting_file_path.write_text(supporting_file_content)
+
+        assert main_file_path.is_file()
+        assert supporting_file_path.is_file()
+
+        JobScriptFiles.delete_from_s3(job_script_id)
+
+        assert not main_file_path.is_file()
+        assert not supporting_file_path.is_file()
+
+    def test_write_to_s3(self, mocked_file_manager_factory):
+
+        job_script_id = 1
+
+        work_dir = mocked_file_manager_factory / JOBSCRIPTS_WORK_DIR / str(job_script_id)
+        output_dir = Path("test-output-dir")
+
+        main_file_name = output_dir / "jobbergate.sh"
+        main_file_content = "test-main-file"
+        main_file_path = work_dir / JOBSCRIPTS_MAIN_FILE_FOLDER / main_file_name
+
+        supporting_file_name = output_dir / "support.sh"
+        supporting_file_content = "test-support-file"
+        supporting_file_path = work_dir / JOBSCRIPTS_SUPPORTING_FILES_FOLDER / supporting_file_name
+
+        JobScriptFiles(
+            main_file_path=main_file_name,
+            files={
+                main_file_name: main_file_content,
+                supporting_file_name: supporting_file_content,
+            },
+        ).write_to_s3(job_script_id)
+
+        assert main_file_path.is_file()
+        assert main_file_path.read_text() == main_file_content
+
+        assert supporting_file_path.is_file()
+        assert supporting_file_path.read_text() == supporting_file_content
+
+    def test_io_integration_with_s3(self, mocked_file_manager_factory):
+
+        job_script_id = 1
+        output_dir = Path("test-output-dir")
+
+        main_file_name = output_dir / "jobbergate.sh"
+        main_file_content = "test-main-file"
+
+        supporting_file_name = output_dir / "support.sh"
+        supporting_file_content = "test-support-file"
+
+        actual_jobscript_files = JobScriptFiles(
+            main_file_path=main_file_name,
+            files={
+                main_file_name: main_file_content,
+                supporting_file_name: supporting_file_content,
+            },
+        )
+
+        actual_jobscript_files.write_to_s3(job_script_id)
+
+        desired_jobscript_files = JobScriptFiles.get_from_s3(job_script_id)
+
+        assert desired_jobscript_files == actual_jobscript_files
