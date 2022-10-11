47ed6ad44693d4c611948fa9b3da96888afc5918
fschuch <felipe@omnivector.solutions>
2022-10-11 17:17:59 -0300

    ARMADA-862 add app/applications/constants

--- jobbergate-api/jobbergate_api/apps/applications/application_files.py
+++ jobbergate-api/jobbergate_api/apps/applications/application_files.py
@@ -10,14 +10,15 @@ from file_storehouse import FileManager
 from loguru import logger
 from pydantic import BaseModel, Field
 
-from jobbergate_api.config import settings
+from jobbergate_api.apps.applications.constants import (
+    APPLICATION_CONFIG_FILE_NAME,
+    APPLICATION_SOURCE_FILE_NAME,
+    APPLICATION_TEMPLATE_FOLDER,
+    APPLICATIONS_WORK_DIR,
+)
 from jobbergate_api.apps.applications.file_validation import perform_all_checks_on_uploaded_files
+from jobbergate_api.config import settings
 from jobbergate_api.s3_manager import IO_TRANSFORMATIONS, file_manager_factory, s3_client
-
-APPLICATIONS_WORK_DIR = "applications"
-APPLICATION_CONFIG_FILE_NAME = "jobbergate.yaml"
-APPLICATION_SOURCE_FILE_NAME = "jobbergate.py"
-APPLICATION_TEMPLATE_FOLDER = "templates"
 
 
 def _get_template_files(file_manager: FileManager) -> Dict[str, str]:
---
+++ jobbergate-api/jobbergate_api/apps/applications/constants.py
@@ -0,0 +1,8 @@
+"""
+Describe constants for the applications module.
+"""
+
+APPLICATIONS_WORK_DIR = "applications"
+APPLICATION_CONFIG_FILE_NAME = "jobbergate.yaml"
+APPLICATION_SOURCE_FILE_NAME = "jobbergate.py"
+APPLICATION_TEMPLATE_FOLDER = "templates"
