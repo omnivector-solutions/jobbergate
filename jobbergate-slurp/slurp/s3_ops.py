7a56b6249279adfc6c7ad55f4e2fc095e38d24fb
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-12 05:10:05 +0000

    Adding tarfile member sanitization to extractall()

--- jobbergate-slurp/slurp/s3_ops.py
+++ jobbergate-slurp/slurp/s3_ops.py
@@ -71,7 +71,29 @@ def get_upload_files_from_tar(s3_obj):
     with tempfile.TemporaryDirectory() as tmpdir:
         tmp_path = Path(tmpdir)
         with tarfile.open(fileobj=BytesIO(s3_obj), mode="r:gz") as tar:
-            tar.extractall(tmp_path)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, tmp_path)
 
         with contextlib.ExitStack() as stack:
             yield [
