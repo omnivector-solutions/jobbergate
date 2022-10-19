b29e2ea097b42565e6306d0e36e6c5fa6b781819
fschuch <felipe@omnivector.solutions>
2022-10-19 18:18:04 -0300

    ARMADA-692 add schema for job properties

--- jobbergate-api/jobbergate_api/apps/job_submissions/schemas.py
+++ jobbergate-api/jobbergate_api/apps/job_submissions/schemas.py
@@ -3,7 +3,7 @@ JobSubmission resource schema.
 """
 from datetime import datetime
 from pathlib import Path
-from typing import Optional
+from typing import Any, Dict, List, Optional
 
 from pydantic import BaseModel, Extra, Field
 
@@ -63,6 +63,86 @@ job_submission_meta_mapper = MetaMapper(
 )
 
 
+class JobProperties(BaseModel, extra=Extra.ignore):
+    """
+    Specialized model for job properties.
+
+    See more details at: https://slurm.schedmd.com/rest_api.html#v0.0.38_job_properties
+    """
+
+    account: Optional[str]
+    account_gather_frequency: Optional[str]
+    argv: Optional[List[str]]
+    array: Optional[str]
+    batch_features: Optional[str]
+    begin_time: Optional[int]
+    burst_buffer: Optional[str]
+    cluster_constraints: Optional[str]
+    comment: Optional[str]
+    constrains: Optional[str]
+    containers: Optional[str]
+    core_specification: Optional[int]
+    cores_per_socket: Optional[int]
+    cpu_binding: Optional[str]
+    cpu_binding_hint: Optional[str]
+    cpu_frequency: Optional[str]
+    cpus_per_gpu: Optional[str]
+    cpus_per_task: Optional[int]
+    current_working_directory: Optional[str]
+    deadline: Optional[str]
+    delay_boot: Optional[int]
+    dependency: Optional[str]
+    distribution: Optional[str]
+    environment: Optional[Dict[Any, Any]]
+    exclusive: Optional[str]
+    get_user_environment: Optional[bool]
+    gress: Optional[str]
+    gress_flags: Optional[str]
+    gpu_binding: Optional[str]
+    gpu_frequency: Optional[str]
+    gpus: Optional[str]
+    gpus_per_node: Optional[str]
+    gpus_per_socket: Optional[str]
+    gpus_per_task: Optional[str]
+    hold: Optional[bool]
+    kill_on_invalid_dependency: Optional[bool]
+    licenses = Optional[str]
+    mail_type: Optional[str]
+    mail_user: Optional[str]
+    mcs_label: Optional[str]
+    memory_binding: Optional[int]
+    memory_per_gpu: Optional[int]
+    memory_per_node: Optional[int]
+    minimum_cpus_per_note: Optional[int]
+    minimum_nodes: Optional[bool]
+    name: Optional[str]
+    nice: Optional[str]
+    no_kill: Optional[bool]
+    nodes: Optional[List[int]]
+    open_mode: Optional[str]
+    partition: Optional[str]
+    priority: Optional[str]
+    qos: Optional[str]
+    requeue: Optional[bool]
+    reservation: Optional[str]
+    signal: Optional[str]
+    sockets_per_node: Optional[int]
+    spread_job: Optional[bool]
+    standard_error: Optional[str]
+    standard_input: Optional[str]
+    standard_output: Optional[str]
+    tasks: Optional[int]
+    tasks_per_core: Optional[int]
+    tasks_per_node: Optional[int]
+    tasks_per_socket: Optional[int]
+    thread_specification: Optional[int]
+    threads_per_core: Optional[int]
+    time_limit: Optional[int]
+    time_minimum: Optional[int]
+    wait_all_nodes: Optional[bool]
+    wckey: Optional[str]
+
+
 class JobSubmissionCreateRequest(BaseModel):
     """
     Request model for creating JobSubmission instances.
@@ -73,6 +153,7 @@ class JobSubmissionCreateRequest(BaseModel):
     job_script_id: int
     execution_directory: Optional[Path]
     client_id: Optional[str]
+    execution_parameters: Optional[JobProperties]
 
     class Config:
         schema_extra = job_submission_meta_mapper
