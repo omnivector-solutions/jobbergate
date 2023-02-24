a54b8255a8349e21012eeb4474579413cc1094fb
fschuch <felipe@omnivector.solutions>
2023-02-24 09:16:10 -0300

    ASP-2810 add auth helper

--- jobbergate-core/jobbergate_core/auth.py
+++ jobbergate-core/jobbergate_core/auth.py
@@ -3,9 +3,11 @@ Utilities for handling auth in Jobbergate.
 """
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import Any, Dict, Optional
+import time
+from typing import Any, Dict, List, Optional
 
 import buzz
+import httpx
 import pendulum
 from jose.jwt import decode
 from loguru import logger
@@ -120,3 +122,113 @@ class Token:
         logger.debug(f"    Token is expired: {is_expired}")
 
         return is_expired
+
+
+@dataclass
+class Authentication:
+    """
+    Class for handling authentication in Jobbergate.
+    """
+
+    cache_path: Path
+    login_domain: str
+    login_audience: str
+    login_client_id: str
+    access_token: Token = field(default_factory=Token, init=False)
+    refresh_token: Token = field(default_factory=Token, init=False)
+    token_list: List = field(init=False, repr=False)
+
+    def __post_init__(self):
+        """
+        Post init method.
+        """
+        self.access_token = Token(cache_path=self.cache_path / "access.token", label="access")
+        self.refresh_token = Token(cache_path=self.cache_path / "refresh.token", label="refresh")
+
+        self.token_list = [self.access_token, self.refresh_token]
+
+        for token in self.token_list:
+            try:
+                token.load_from_cache()
+            except Exception:
+                logger.warning(f"Error while loading {token.label} token from cache")
+
+    def __call__(self, request):
+        """
+        Authenticate the request.
+        """
+        logger.debug("Authenticating request")
+        request.headers["Authorization"] = f"Bearer {self.access_token.content}"
+        return request
+
+    def fetch_tokens(self):
+        """
+        Fetch the tokens.
+        """
+        # logger.debug("Fetching tokens")
+        # self.access_token = Token(
+        #     cache_path=self.cache_path / "access_token",
+        #     label="access",
+        # )
+        # self.refresh_token = Token(
+        #     cache_path=self.cache_path / "refresh_token",
+        #     label="refresh",
+        # )
+
+        # self.access_token.load_from_cache()
+        # self.refresh_token.load_from_cache()
+
+    def login(self):
+        """
+        Login to Jobbergate.
+        """
+        response = httpx.post(
+            f"{self.login_domain}/protocol/openid-connect/auth/device",
+            data=dict(
+                client_id=self.login_client_id,
+                grant_type="client_credentials",
+                audience=self.login_audience,
+            ),
+        )
+        device_code_data = response.json()
+        verification_url = device_code_data["verification_uri_complete"]
+        wait_interval = device_code_data["interval"]
+        device_code = device_code_data["device_code"]
+
+        print(f"Login Here: {verification_url}")
+        while True:
+            time.sleep(wait_interval)
+            response = httpx.post(
+                f"{self.login_domain}/protocol/openid-connect/token",
+                data=dict(
+                    grant_type="urn:ietf:params:oauth:grant-type:device_code",
+                    device_code=device_code,
+                    client_id=self.login_client_id,
+                ),
+            )
+            try:
+                response.raise_for_status()  # Will throw an exception if response isn't in 200s
+                response_data = response.json()
+                return (
+                    response_data["access_token"],
+                    response_data["refresh_token"],
+                )
+            except Exception:
+                pass
+
+    def refresh_tokens(self):
+        """
+        Refresh the tokens.
+        """
+        response = httpx.post(
+            f"{self.login_domain}/protocol/openid-connect/token",
+            data=dict(
+                client_id=self.login_client_id,
+                audience=self.login_audience,
+                grant_type="refresh_token",
+                refresh_token=self.refresh_token.content,
+            ),
+        )
+        refreshed_data = response.json()
+        access_token = refreshed_data["access_token"]
+        return access_token
