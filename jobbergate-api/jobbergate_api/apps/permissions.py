f04074c118b38b4590e52a60454645a1fd4e4279
Tucker Beck <tucker.beck@gmail.com>
2023-06-22 12:48:42 -0700

    Added owner check to applicaitons route

--- jobbergate-api/jobbergate_api/apps/permissions.py
+++ jobbergate-api/jobbergate_api/apps/permissions.py
@@ -3,6 +3,11 @@ Provide a module that describes permissions in the API.
 """
 
 from enum import Enum
+from typing import Union
+
+from fastapi import HTTPException, status
+from loguru import logger
+from pydantic import EmailStr
 
 
 class Permissions(str, Enum):
@@ -16,3 +21,22 @@ class Permissions(str, Enum):
     JOB_SCRIPTS_EDIT = "jobbergate:job-scripts:edit"
     JOB_SUBMISSIONS_VIEW = "jobbergate:job-submissions:view"
     JOB_SUBMISSIONS_EDIT = "jobbergate:job-submissions:edit"
+
+
+def check_owner(
+        owner_email: Union[str, EmailStr],
+        requester_email: Union[str, EmailStr],
+        entity_id: int,
+        entity_name: str,
+):
+    """
+    Generic method to assert ownership of an entity and raise a 403 exception with message on failure.
+    """
+    if not requester_email == owner_email:
+        message = (
+            f"User {requester_email} does not own {entity_name} {entity_id}. "
+            f"Only the {entity_name} owner ({owner_email}) "
+            "can modify this {entity_name}."
+        )
+        logger.error(message)
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=message)
