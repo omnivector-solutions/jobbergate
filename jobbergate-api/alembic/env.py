f20475e61241a35b49000e4fc750d2c95a373bba
Tucker Beck <tucker.beck@gmail.com>
2023-07-28 12:16:07 -0700

    Fixed migrations

--- jobbergate-api/alembic/env.py
+++ jobbergate-api/alembic/env.py
@@ -7,9 +7,9 @@ from jobbergate_api.apps.job_scripts import models  # noqa # must be imported fo
 from jobbergate_api.apps.job_submissions import models  # noqa # must be imported for metadata to work
 from jobbergate_api.apps.models import Base
 from jobbergate_api.apps.job_script_templates import models  # noqa # must be imported for metadata to work
-from jobbergate_api.metadata import (
-    metadata as jobbergate_api_metadata,
-)  # noqa # must be imported for metadata to work
+# from jobbergate_api.metadata import (
+#    metadata as jobbergate_api_metadata,
+# )  # noqa # must be imported for metadata to work
 from jobbergate_api.storage import build_db_url
 
 # this is the Alembic Config object, which provides
@@ -45,7 +45,7 @@ def run_migrations_offline():
 
     """
     context.configure(
-        url=build_db_url(),
+        url=build_db_url(asynchronous=False),
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
@@ -63,7 +63,7 @@ def run_migrations_online():
 
     """
     ini_section = config.get_section(config.config_ini_section)
-    ini_section["sqlalchemy.url"] = build_db_url()
+    ini_section["sqlalchemy.url"] = build_db_url(asynchronous=False)
     connectable = engine_from_config(
         ini_section,
         prefix="sqlalchemy.",
