4aa8001391a0ed154ff8e5fcbbced2b5d147e73e
Felipe N. Schuch <me@fschuch.com>
2023-08-29 16:49:22 -0300

    fix-api: patch organization id on multi tenancy

--- jobbergate-api/tests/test_security.py
+++ jobbergate-api/tests/test_security.py
@@ -2,15 +2,24 @@
 Test the security module.
 """
 from unittest.mock import patch
-
-from jobbergate_api import security
+import pytest
+from jobbergate_api.security import (
+    IdentityPayload,
+    TokenPayload,
+    lockdown_with_identity,
+    HTTPException,
+    settings,
+    get_domain_configs,
+)
 
 
 def test_get_domain_configs__loads_only_base_settings():
     """Check if the correct domain configuration is loaded when only one domain is provided."""
-    with patch.object(security.settings, "ARMASEC_DOMAIN", new="foo.io"):
-        with patch.object(security.settings, "ARMASEC_AUDIENCE", new="https://bar.dev"):
-            domain_configs = security.get_domain_configs()
+    with (
+        patch.object(settings, "ARMASEC_DOMAIN", new="foo.io"),
+        patch.object(settings, "ARMASEC_AUDIENCE", new="https://bar.dev"),
+    ):
+        domain_configs = get_domain_configs()
 
     assert len(domain_configs) == 1
     first_config = domain_configs.pop()
@@ -20,23 +29,27 @@ def test_get_domain_configs__loads_only_base_settings():
 
 def test_get_domain_configs__loads_admin_settings_if_all_are_present():
     """Check if the correct domain configuration is loaded when two domains are provided."""
-    with patch.object(security.settings, "ARMASEC_DOMAIN", new="foo.io"):
-        with patch.object(security.settings, "ARMASEC_AUDIENCE", new="https://bar.dev"):
-            with patch.object(security.settings, "ARMASEC_ADMIN_DOMAIN", new="admin.io"):
-                domain_configs = security.get_domain_configs()
+    with (
+        patch.object(settings, "ARMASEC_DOMAIN", new="foo.io"),
+        patch.object(settings, "ARMASEC_AUDIENCE", new="https://bar.dev"),
+        patch.object(settings, "ARMASEC_ADMIN_DOMAIN", new="admin.io"),
+    ):
+        domain_configs = get_domain_configs()
 
     assert len(domain_configs) == 1
     first_config = domain_configs.pop()
     assert first_config.domain == "foo.io"
     assert first_config.audience == "https://bar.dev"
 
-    with patch.object(security.settings, "ARMASEC_DOMAIN", new="foo.io"):
-        with patch.object(security.settings, "ARMASEC_AUDIENCE", new="https://bar.dev"):
-            with patch.object(security.settings, "ARMASEC_ADMIN_DOMAIN", new="admin.io"):
-                with patch.object(security.settings, "ARMASEC_ADMIN_AUDIENCE", new="https://admin.dev"):
-                    with patch.object(security.settings, "ARMASEC_ADMIN_MATCH_KEY", new="foo"):
-                        with patch.object(security.settings, "ARMASEC_ADMIN_MATCH_VALUE", new="bar"):
-                            domain_configs = security.get_domain_configs()
+    with (
+        patch.object(settings, "ARMASEC_DOMAIN", new="foo.io"),
+        patch.object(settings, "ARMASEC_AUDIENCE", new="https://bar.dev"),
+        patch.object(settings, "ARMASEC_ADMIN_DOMAIN", new="admin.io"),
+        patch.object(settings, "ARMASEC_ADMIN_AUDIENCE", new="https://admin.dev"),
+        patch.object(settings, "ARMASEC_ADMIN_MATCH_KEY", new="foo"),
+        patch.object(settings, "ARMASEC_ADMIN_MATCH_VALUE", new="bar"),
+    ):
+        domain_configs = get_domain_configs()
 
     assert len(domain_configs) == 2
     (first_config, second_config) = domain_configs
@@ -45,3 +58,48 @@ def test_get_domain_configs__loads_admin_settings_if_all_are_present():
     assert second_config.domain == "admin.io"
     assert second_config.audience == "https://admin.dev"
     assert second_config.match_keys == dict(foo="bar")
+
+
+def test_lockdown_with_identity__success():
+    """Check if the lockdown_with_identity decorator returns the correct identity."""
+    token_raw_data = dict(sub="dummy-sub")
+    token_payload = TokenPayload.parse_obj(token_raw_data)
+    lock = lockdown_with_identity()
+
+    actual_identity = lock(token_payload)
+    expected_identity = IdentityPayload.parse_obj(token_raw_data)
+
+    assert actual_identity == expected_identity
+
+
+@pytest.mark.parametrize("opt_name", ["ensure_email", "ensure_organization", "ensure_client_id"])
+def test_lockdown_with_identity_ensure_fields__success(opt_name):
+    """Check if the lockdown_with_identity decorator returns the correct identity."""
+    token_raw_data = dict(
+        sub="dummy-sub",
+        client_id="dummy-client-id",
+        email="dummy-email@pytest.com",
+        organization_id="dummy-organization-id",
+    )
+    token_payload = TokenPayload.parse_obj(token_raw_data)
+
+    kwargs = {opt_name: True}
+    lock = lockdown_with_identity(**kwargs)
+
+    actual_identity = lock(token_payload)
+    expected_identity = IdentityPayload.parse_obj(token_raw_data)
+
+    assert actual_identity == expected_identity
+
+
+@pytest.mark.parametrize("opt_name", ["ensure_email", "ensure_organization", "ensure_client_id"])
+def test_lockdown_with_identity_ensure_fields__raises_error(opt_name):
+    """Check if the lockdown_with_identity decorator raises an error when the identity is missing a field."""
+    token_raw_data = dict(sub="dummy-sub")
+    token_payload = TokenPayload.parse_obj(token_raw_data)
+    kwargs = {opt_name: True}
+    lock = lockdown_with_identity(**kwargs)
+
+    with pytest.raises(HTTPException) as exc_info:
+        lock(token_payload)
+    assert exc_info.value.status_code == 400
