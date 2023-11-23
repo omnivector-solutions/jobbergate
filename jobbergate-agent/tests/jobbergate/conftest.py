cf92ba813400c54657095448a5224874f3d431fa
Juliana Karoline <julianaklulo@gmail.com>
2023-11-23 16:48:12 -0300

    ASp-4245 Add fixture to test supporting files

--- jobbergate-agent/tests/jobbergate/conftest.py
+++ jobbergate-agent/tests/jobbergate/conftest.py
@@ -26,11 +26,33 @@ def dummy_template_source():
 
 @pytest.fixture
 def dummy_job_script_files():
+    """
+    Provide a fixture that returns a list of dummy job script files.
+    """
     return [
         {
             "parent_id": 1,
             "filename": "application.sh",
             "file_type": "ENTRYPOINT",
+        },
+    ]
+
+
+@pytest.fixture
+def dummy_job_script_with_supporting_files():
+    """
+    Provide a fixture that returns a list of dummy job script files with supporting files.
+    """
+    return [
+        {
+            "parent_id": 1,
+            "filename": "application.sh",
+            "file_type": "ENTRYPOINT",
+        },
+        {
+            "parent_id": 1,
+            "filename": "input.txt",
+            "file_type": "SUPPORT",
         },
     ]
 
@@ -45,5 +67,19 @@ def dummy_pending_job_submission_data(dummy_job_script_files, tmp_path):
         name="sub1",
         owner_email="email1@dummy.com",
         job_script={"files": dummy_job_script_files},
+        slurm_job_id=13,
+    )
+
+
+@pytest.fixture
+def dummy_pending_job_submission_with_supporting_files_data(dummy_job_script_with_supporting_files, tmp_path):
+    """
+    Provide a fixture that returns a dict that is compatible with PendingJobSubmission and has supporting files.
+    """
+    return dict(
+        id=1,
+        name="sub1",
+        owner_email="email1.@dummy.com",
+        job_script={"files": dummy_job_script_with_supporting_files},
         slurm_job_id=13,
     )
