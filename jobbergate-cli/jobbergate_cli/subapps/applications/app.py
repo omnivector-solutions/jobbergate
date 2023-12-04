556d1d3bd9e32143ba88c3fe6106c33a1cb97938
Juliana Karoline <julianaklulo@gmail.com>
2023-12-04 16:11:35 -0300

    ASP-3966 Update imports for handle_pagination

--- jobbergate-cli/jobbergate_cli/subapps/applications/app.py
+++ jobbergate-cli/jobbergate_cli/subapps/applications/app.py
@@ -10,7 +10,7 @@ import typer
 
 from jobbergate_cli.constants import OV_CONTACT, SortOrder
 from jobbergate_cli.exceptions import handle_abort
-from jobbergate_cli.pagination import handle_pagination
+from jobbergate_cli.subapps.pagination import handle_pagination
 from jobbergate_cli.render import StyleMapper, render_single_result, terminal_message
 from jobbergate_cli.requests import make_request
 from jobbergate_cli.schemas import JobbergateContext
