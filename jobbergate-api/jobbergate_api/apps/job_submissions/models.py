a40a7166af02788e7ae1a0e7400044615b24d0c4
Felipe N. Schuch <me@fschuch.com>
2024-02-26 16:47:06 -0300

    feat!(api): remove database column JobSubmission.execution_parameters

--- jobbergate-api/jobbergate_api/apps/job_submissions/models.py
+++ jobbergate-api/jobbergate_api/apps/job_submissions/models.py
@@ -4,10 +4,7 @@ Database model for the JobSubmission resource.
 
 from __future__ import annotations
 
-from typing import Any
-
-from sqlalchemy import Enum, ForeignKey, Integer, String, text
-from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy import Enum, ForeignKey, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship, selectinload
 from sqlalchemy.sql.expression import Select
 
@@ -34,7 +31,6 @@ class JobSubmission(CrudMixin, Base):
         client_id: The id of the custer this submission runs on.
         status: The status of the job submission.
         report_message: The message returned by the job.
-        execution_parameters: The properties of the job.
 
     See Mixin class definitions for other columns
     """
@@ -60,14 +56,6 @@ class JobSubmission(CrudMixin, Base):
         index=True,
     )
     report_message: Mapped[str] = mapped_column(String, nullable=True)
-
-    execution_parameters: Mapped[dict[str, Any]] = mapped_column(
-        "template_vars",
-        JSONB,
-        nullable=False,
-        default=text("'{}'::jsonb"),
-        server_default=text("'{}'::jsonb"),
-    )
 
     job_script: Mapped[JobScript] = relationship(
         "JobScript",
