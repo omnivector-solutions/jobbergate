5bec79e61a73bc368d638fac95355bffc792c359
Felipe N. Schuch <me@fschuch.com>
2024-03-06 17:48:54 -0300

    modified simple-application example
    It bugs me that the absolute path of the resulting file ends up hardcoded there.
    It results in permission denied errors when different users try the same example.
    It should depend on the working directory the job is submitted to.

--- examples/simple-application/jobbergate.py
+++ examples/simple-application/jobbergate.py
@@ -6,7 +6,7 @@ class JobbergateApplication(JobbergateApplicationBase):
 
     def mainflow(self, data=None):
         if data is None:
-            data=dict()
+            data = dict()
         data["nextworkflow"] = "subflow"
         return [
             Text("foo", message="gimme the foo!", default="foo"),
@@ -15,8 +15,8 @@ class JobbergateApplication(JobbergateApplicationBase):
 
     def subflow(self, data=None):
         if data is None:
-            data=dict()
+            data = dict()
         return [
             Text("baz", message="gimme the baz!", default="zab"),
-            Text("workdir", message="gimme the workdir!", default="/nfs"),
+            Text("filename", message="gimme the filename!", default="dummy-result.txt"),
         ]
