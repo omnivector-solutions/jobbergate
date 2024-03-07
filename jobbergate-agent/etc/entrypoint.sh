d49e2d35239c6fd11a9b39ad280a487aad3de638
Felipe N. Schuch <me@fschuch.com>
2024-03-07 15:08:51 -0300

    fix jobbergate-compose for new agent

--- jobbergate-agent/etc/entrypoint.sh
+++ jobbergate-agent/etc/entrypoint.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
 set -e
+
+echo "---> Starting the MUNGE Authentication service (munged) ..."
+service munge start
+
+echo "---> Waiting for slurmctld to become active before starting slurmd..."
+
+until 2>/dev/null >/dev/tcp/slurmctld/6817
+do
+    echo "-- slurmctld is not available.  Sleeping ..."
+    sleep 2
+done
+echo "-- slurmctld is now active ..."
+
+echo "---> Starting the Slurm Node Daemon (slurmd) ..."
+/usr/sbin/slurmd -Dvvv
+
+echo "---> Starting Jobbergate-agent ..."
 cd /app
 poetry install
 poetry run jg-run
