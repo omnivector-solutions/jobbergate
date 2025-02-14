c2f86e8f01d10d2c006db740dd051f15e36343f5
zsinx6 <zsinx6@users.noreply.github.com>
2025-02-14 11:46:08 -0300

    use correct user home

--- jobbergate-agent/jobbergate_agent/jobbergate/submit.py
+++ jobbergate-agent/jobbergate_agent/jobbergate/submit.py
@@ -244,7 +244,7 @@ async def submit_job_script(
         logger.debug(f"Using local slurm user '{username}' for job submission")
         subprocess_handler = SubprocessAsUserHandler(username)
 
-    submit_dir = pending_job_submission.execution_directory or Path.home().resolve()
+    submit_dir = pending_job_submission.execution_directory or Path(f"/home/{username}")
     async with handle_errors_async(
         "Execution directory is invalid", raise_exc_class=JobSubmissionError, do_except=_reject_handler
     ):
