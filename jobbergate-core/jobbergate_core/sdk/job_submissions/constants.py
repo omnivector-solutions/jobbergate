acaea7e9497901c0263b402bea9a4fa0bdd3f881
Felipe N. Schuch <me@fschuch.com>
2025-03-18 13:24:42 -0300

    wip

---
+++ jobbergate-core/jobbergate_core/sdk/job_submissions/constants.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class JobSubmissionStatus(str, Enum):
+    """
+    Defines the set of possible statuses for a Job Submission.
+    """
+
+    CREATED = "CREATED"
+    SUBMITTED = "SUBMITTED"
+    REJECTED = "REJECTED"
+    DONE = "DONE"
+    ABORTED = "ABORTED"
