68cea8fef2f4d67d145e438c3dd8417226c257c9
Juliana Karoline <julianaklulo@gmail.com>
2025-05-08 14:00:22 -0300

    feat(api): expand logic to auto cleanup for job submissions

--- jobbergate-api/jobbergate_api/apps/job_submissions/services.py
+++ jobbergate-api/jobbergate_api/apps/job_submissions/services.py
@@ -1,9 +1,24 @@
 """Services for the job_submissions resource, including module specific business logic."""
 
+from typing import NamedTuple
+
+from loguru import logger
+from pendulum.datetime import DateTime as PendulumDateTime
+from sqlalchemy import delete, select, update
 from sqlalchemy.sql.expression import Select
 
 from jobbergate_api.apps.job_submissions.models import JobSubmission
 from jobbergate_api.apps.services import CrudService
+from jobbergate_api.config import settings
+
+
+class AutoCleanResponse(NamedTuple):
+    """
+    Named tuple for the response of auto_clean_unused_job_scripts.
+    """
+
+    archived: set[int]
+    deleted: set[int]
 
 
 class JobSubmissionService(CrudService):
@@ -37,6 +52,48 @@ class JobSubmissionService(CrudService):
         if filter_slurm_job_ids:
             query = query.where(JobSubmission.slurm_job_id.in_(filter_slurm_job_ids))
         return query
+
+    async def auto_clean_unused_job_submissions(self) -> AutoCleanResponse:
+        """
+        Automatically clean unused job submissions depending on a threshold.
+
+        Based on the last time each job submission was updated, this will archive job
+        submissions that were unarchived and delete job submissions that were archived.
+        """
+        result = AutoCleanResponse(archived=set(), deleted=set())
+
+        if days_to_delete := settings.AUTO_CLEAN_JOB_SUBMISSIONS_DAYS_TO_DELETE:
+            threshold = PendulumDateTime.utcnow().subtract(days=days_to_delete).naive()
+            query_unused_job_submissions = select(self.model_type.id).where(
+                self.model_type.is_archived.is_(True), self.model_type.updated_at < threshold
+            )
+
+            delete_query = (
+                delete(self.model_type)  # type: ignore
+                .where(self.model_type.id.in_(query_unused_job_submissions))
+                .returning(self.model_type.id)
+            )
+            deleted = await self.session.execute(delete_query)
+            result.deleted.update(row[0] for row in deleted.all())
+        logger.debug(f"Job submissions deleted: {result.deleted}")
+
+        if days_to_archive := settings.AUTO_CLEAN_JOB_SUBMISSIONS_DAYS_TO_ARCHIVE:
+            threshold = PendulumDateTime.utcnow().subtract(days=days_to_archive).naive()
+            query_unused_job_submissions = select(self.model_type.id).where(
+                self.model_type.is_archived.is_(False), self.model_type.updated_at < threshold
+            )
+
+            update_query = (
+                update(self.model_type)  # type: ignore
+                .where(self.model_type.id.in_(query_unused_job_submissions))
+                .values(is_archived=True)
+                .returning(self.model_type.id)
+            )
+            archived = await self.session.execute(update_query)
+            result.archived.update(row[0] for row in archived.all())
+        logger.debug(f"Job submissions marked as archived: {result.archived}")
+
+        return result
 
 
 class JobProgressService(CrudService):
