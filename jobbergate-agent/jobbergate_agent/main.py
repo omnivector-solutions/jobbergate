c5e194242b318a89b0ba35f9eec6c4be210a27cb
Felipe N. Schuch <felipe@omnivector.solutions>
2026-02-17 10:33:56 -0300

    fix(agent): scheduler on python 3.14

--- jobbergate-agent/jobbergate_agent/main.py
+++ jobbergate-agent/jobbergate_agent/main.py
@@ -1,20 +1,29 @@
 import asyncio
 
 from jobbergate_agent.utils.logging import logger
-from jobbergate_agent.utils.scheduler import init_scheduler, shut_down_scheduler
+from jobbergate_agent.utils.scheduler import init_scheduler, scheduler, shut_down_scheduler
 from jobbergate_agent.utils.sentry import init_sentry
+
+
+async def helper():
+    """
+    Based on example from the scheduler documentation:
+
+    https://github.com/agronholm/apscheduler/blob/3.x/examples/schedulers/asyncio_.py
+    """
+    init_scheduler()
+    while True:
+        await asyncio.sleep(1000)
 
 
 def main():
     logger.info("Starting Jobbergate-agent")
     init_sentry()
-    scheduler = init_scheduler()
-
-    try:
-        asyncio.get_event_loop().run_forever()
-    except (KeyboardInterrupt, SystemExit):
-        shut_down_scheduler(scheduler)
-
-
-if __name__ == "__main__":
-    main()
+    with asyncio.Runner() as runner:
+        try:
+            runner.run(helper())
+        except (KeyboardInterrupt, SystemExit):
+            logger.info("Jobbergate-agent is shutting down...")
+        finally:
+            shut_down_scheduler(scheduler, wait=False)
+    logger.info("Jobbergate-agent has been stopped")
